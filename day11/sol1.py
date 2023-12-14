37961db7af47ec2bef56f0be4a3b56f7e951e559
Ferran <ferrandelmoral@gmail.com>
2023-12-14 13:28:58 +0100

    part 1 day 11

--- day11/sol1.py
+++ day11/sol1.py
@@ -1,8 +1,17 @@
-with open("day11/data/test.txt", "r") as f:
+from __future__ import annotations
+
+with open("day11/data/input.txt", "r") as f:
     data = [line.strip() for line in f.readlines()]
 
 for i, row in enumerate(data):
     data[i] = list(row)
+
+class Galaxy:
+    def __init__(self, position: tuple[int, int]) -> None:
+        self.position = position
+
+    def distance_to(self, other: Galaxy) -> int:
+        return abs(self.position[0] - other.position[0]) + abs(self.position[1] - other.position[1])
 
 def expand_universe(data: list[str]) -> list[str]:
     # columns
@@ -33,6 +42,27 @@ def expand_universe(data: list[str]) -> list[str]:
 
 data = expand_universe(data)
 
+galaxies= []
+for i, row in enumerate(data):
+    for j, cell in enumerate(row):
+        if cell == "#":
+            galaxies.append(Galaxy((i, j)))
 
-for line in data:
-    print(line)
+# find distance of each galaxy with each other only once
+distances = {}
+for galaxy in galaxies:
+    for other in galaxies:
+        if galaxy == other:
+            continue
+        pair = galaxy.position, other.position
+        if pair in distances:
+            continue
+        rev_pair = other.position, galaxy.position
+        distances[rev_pair] = galaxy.distance_to(other)
+        
+
+total_dist = 0
+for pair, distance in distances.items():
+    total_dist += distance
+
+print(total_dist)
