f140e3d1e4c72bb5397966817b48ccb227504044
Ferran <ferrandelmoral@gmail.com>
2023-12-14 12:09:38 +0100

    fixed expansion

--- day11/sol1.py
+++ day11/sol1.py
@@ -1,15 +1,8 @@
 with open("day11/data/test.txt", "r") as f:
     data = [line.strip() for line in f.readlines()]
 
-# rows
-i = 0
-while i < len(data):
-    data[i] = list(data[i])
-    if "#" not in data[i]:
-        data.insert(i, data[i])
-        i += 2
-    else:
-        i += 1
+for i, row in enumerate(data):
+    data[i] = list(row)
 
 # columns
 j = 0
@@ -18,11 +11,20 @@ while j < len(data[0]):
     if "#" not in col:
         i = 0
         while i < len(col):
-            data[i].insert(j, col[i])  # this wrong
+            data[i].insert(j, col[i])
             i += 1
         j += 2
     else:
         j += 1
+
+# rows
+i = 0
+while i < len(data):
+    if "#" not in data[i]:
+        data.insert(i, data[i])
+        i += 2
+    else:
+        i += 1
 
 for line in data:
     print(line)
